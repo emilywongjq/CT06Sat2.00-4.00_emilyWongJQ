961b2bbd6f3f9ad5798f5bb89bd47fff97274e4d
Emily Wong <emily.wongjq@gmail.com>
2025-07-12 07:31:54 +0000

    Jul 12, 2025, 7:31 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -9,6 +9,6 @@ function draw(){
     fill(255,251,0);
     ellipse(300,100,100,100)
     fill(0,255,39);
-    ellipse(300,50,200,100)
+    ellipse(300,50,100,100)
     // Todo: Challenge 3 Traffic Light
 }
