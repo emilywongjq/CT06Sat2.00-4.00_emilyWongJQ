7a5a901f4828f66e87a0f4278affe478c3f60f54
Emily Wong <emily.wongjq@gmail.com>
2025-07-12 07:31:02 +0000

    Jul 12, 2025, 7:31 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -5,7 +5,7 @@ function setup(){
 
 function draw(){
     fill(255, 0, 0);
-    ellipse(300,200,200,200);
+    ellipse(300,200,100,200);
     fill(255,251,0);
     eclipse(300,100,200,200)
     fill(0,255,39);
