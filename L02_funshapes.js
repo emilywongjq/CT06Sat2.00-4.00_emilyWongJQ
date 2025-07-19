2081535f798836898d4b8c05107718defec757eb
Emily Wong <emily.wongjq@gmail.com>
2025-07-19 07:27:31 +0000

    Jul 19, 2025, 7:27 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -32,7 +32,7 @@ function draw() {
   // triangle(20,40,75,40,57,32);
   quad(20,20,80,20,80,80,20,80);
   quad(120,30,180,30,180,70,120,70);
-  quad(50,162,86,150,50,130,138,14,150);
+  quad(50,162,86,150,50,138,14,150);
   // Challenge 5: A square face using quad() function
 
   // Challenge 6: Add some colours to the drawings in the above challenges
