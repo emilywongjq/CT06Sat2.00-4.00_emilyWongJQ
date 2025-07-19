2c1d7b6c02c7c4dcdb835910b522b34e30be57e6
Emily Wong <emily.wongjq@gmail.com>
2025-07-19 07:11:30 +0000

    Jul 19, 2025, 7:11 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -18,7 +18,7 @@ function draw() {
     circle(300,400,400);
     circle(115,200,150);
     circle(485,200,150);
-    circle(100,100,12)
+    circle(100,300,50)
   // Challenge 3: A house using rect() function
 
   // Challenge 4: A rocketship using triangle() function
