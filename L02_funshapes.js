83a641ff5d151eb716cb1ad79bcc4926367deabc
Emily Wong <emily.wongjq@gmail.com>
2025-07-19 06:50:09 +0000

    Jul 19, 2025, 6:50 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -7,7 +7,7 @@ function setup() {
 
 function draw() {
   // Challenge 1: An eye using ellipse() function
-  ellipse()
+  ellipse(100,100,80,50)
   // Challenge 2: A face using circle() function
 
   // Challenge 3: A house using rect() function
