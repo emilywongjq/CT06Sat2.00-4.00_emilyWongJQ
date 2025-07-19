fb38afdc25272944bf47c14cb5c7bcd846faf853
Emily Wong <emily.wongjq@gmail.com>
2025-07-19 07:25:14 +0000

    Jul 19, 2025, 7:25 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -28,8 +28,9 @@ function draw() {
   //Give each corner a unique radius
 
   // Challenge 4: A rocketship using triangle() function
-  triangle(100,150,150,90,90,75);
-  triangle(20,40,75,40,57,32);
+  // triangle(100,150,150,90,90,75);
+  // triangle(20,40,75,40,57,32);
+  quad
   // Challenge 5: A square face using quad() function
 
   // Challenge 6: Add some colours to the drawings in the above challenges
