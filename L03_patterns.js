dbdcd78657ca8ea4511dde2cb5216edc3c1a4aa5
Emily Wong <emily.wongjq@gmail.com>
2025-08-02 07:09:12 +0000

    Aug 2, 2025, 7:09 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -44,6 +44,7 @@ function draw() {
   let totalWidth = circleDiameter * numOfCircles;
   let startX = (width - totalWidth)/2 + circleDiameter/2;
   let startY = (height - totalWidth)/2 + circleDiameter/2;
+  
   for (let i = 0; i< numOfCircles; i++){
     let x = startX + i * circleDiameter;
     ellipse(x ,rowY , height/2,circleDiameter , circleDiameter);
