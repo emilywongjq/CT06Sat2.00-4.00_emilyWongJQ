9ea0e7a058ea25abf73f52d03803df3fae02a5db
Emily Wong <emily.wongjq@gmail.com>
2025-08-16 07:20:28 +0000

    Aug 16, 2025, 7:20 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -99,25 +99,25 @@
 //     circleSize += 2
 // }
 //____________________________________________
-let shapeColor ="blue";
-let circleSize = 20;
-function setup(){
-    createCanvas(400,400)
-    background(200);
-}
+// let shapeColor ="blue";
+// let circleSize = 20;
+// function setup(){
+//     createCanvas(400,400)
+//     background(200);
+// }
 
-function mouseDragged(){
-    fill(random(255), random(255),random(255));
-    circleSize = random(10,100);
-    ellipse(mouseX,mouseY,circleSize,circleSize);
-}
-function mousePressed(){
-    shapeColor = color(random(255), random(255),random(255));
-}
+// function mouseDragged(){
+//     fill(random(255), random(255),random(255));
+//     circleSize = random(10,100);
+//     ellipse(mouseX,mouseY,circleSize,circleSize);
+// }
+// function mousePressed(){
+//     shapeColor = color(random(255), random(255),random(255));
+// }
 
-function mouseReleased(){
-    shapeColor = color(random(255), random(255),random(255));
-}
-function mouseMoved(){
-    circleSize += 2
-}
+// function mouseReleased(){
+//     shapeColor = color(random(255), random(255),random(255));
+// }
+// function mouseMoved(){
+//     circleSize += 2
+// }
