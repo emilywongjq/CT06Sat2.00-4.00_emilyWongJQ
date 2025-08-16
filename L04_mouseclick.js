cf80d7130a87277e5f713a87ff5d3f0f30c2835a
Emily Wong <emily.wongjq@gmail.com>
2025-08-16 07:09:14 +0000

    Aug 16, 2025, 7:09 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -98,4 +98,27 @@
 // function mouseMoved(){
 //     circleSize += 2
 // }
+//____________________________________________
+let shapeColor ="blue";
+let circleSize = 20;
+function setup(){
+    createCanvas(400,400)
+    background(200);
+}
 
+function draw(){
+    fill(shapeColor);
+    ellipse(mouseX,mouseY,circleSize,circleSize)
+}
+
+function mousePressed(){
+    shapeColor = color(random(255), random(255),random(255))
+    circleSize = 20;
+}
+
+function mouseReleased(){
+    shapeColor = color(random(255), random(255),random(255))
+}
+function mouseMoved(){
+    circleSize += 2
+}
