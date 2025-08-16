a83ffa3449496088f82300d62262cdaf6dbcab0e
Emily Wong <emily.wongjq@gmail.com>
2025-08-16 06:13:00 +0000

    Aug 16, 2025, 6:13 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,2 +1,18 @@
 // write your codes here
 
+function setup(){
+    createCanvas(1000, 800);
+    background(220);
+}
+
+function draw(){
+    fill(255, 0, 0);
+    ellipse(300,100,100,100);
+
+    fill(255,251,0);
+    ellipse(300,200,100,100)
+    
+    fill(0,255,39);
+    ellipse(300,300,100,100)
+    // Todo: Challenge 3 Traffic Light
+}
