f537ac1f5abbdce1aa42b101bb8652042f40e0a4
Emily Wong <emily.wongjq@gmail.com>
2025-08-16 06:31:23 +0000

    Aug 16, 2025, 6:31 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -9,7 +9,7 @@ function draw(){
     ellipse(200,200,80,80);
 }
 function mousePressed(){
-    shapecolour = colour(random(255),random(255))
+    shapecolour = colour(random(255),random(255),random(255))
 }
 function mouseReleased(){
     shapecolour = colour(random(255),random(255))
