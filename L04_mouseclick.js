eaf76eb728d8ce72cd2cbb42e32590de9b84f7eb
Emily Wong <emily.wongjq@gmail.com>
2025-08-16 06:56:50 +0000

    Aug 16, 2025, 6:56 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -75,3 +75,22 @@
 //     shapeColor = color(random(255), random(255),random(255))
 // }
 //____________________________________________
+let shapeColor ="blue";
+
+function setup(){
+    createCanvas(400,400)
+    background(200);
+}
+
+function draw(){
+    fill(shapeColor);
+    ellipse(200,200,80,80)
+}
+
+function mousePressed(){
+    shapeColor = color(random(255), random(255),random(255))
+}
+
+function mouseReleased(){
+    shapeColor = color(random(255), random(255),random(255))
+}
