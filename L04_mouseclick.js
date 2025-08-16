af82f52c22e7603f37d978c3b5b5f4a67106a844
Emily Wong <emily.wongjq@gmail.com>
2025-08-16 06:45:48 +0000

    Aug 16, 2025, 6:45 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -37,7 +37,9 @@ let height = random(400);
 function draw(){
     fill(shapeColor);
     //change
-    if(mouseIsPressed);
+    if(mouseIsPressed){
+        
+    }
     triangle(x1,y1,x2,width,height);
 }
 
