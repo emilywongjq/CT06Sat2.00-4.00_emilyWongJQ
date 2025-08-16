678e5bcd24c0fad4800261e1e1ad6624fb3b1bed
Emily Wong <emily.wongjq@gmail.com>
2025-08-16 06:51:34 +0000

    Aug 16, 2025, 6:51 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -22,31 +22,31 @@
 // }
 //_______________________________________________________________________________
 //Challenge 1!
-let shapeColor ="blue";
+// let shapeColor ="blue";
 
-function setup(){
-    createCanvas(400,400)
-    background(200);
-}
-function draw(){
-    fill(shapeColor);
-    //change
-    if(mouseIsPressed){
-        triangle(x1,y1,x2,x3,y3);
-    }
-}
+// function setup(){
+//     createCanvas(400,400)
+//     background(200);
+// }
+// function draw(){
+//     fill(shapeColor);
+//     //change
+//     if(mouseIsPressed){
+//         triangle(x1,y1,x2,x3,y3);
+//     }
+// }
 
-function mousePressed(){
-    shapeColor = color(random(255), random(255),random(255))
-    let x1 = random(width);
-    let y1 = random(height);
-    let x2 = random(width);
-    let y2 = random(height);
-    let x3 = random(width);
-    let y3 = random(height);
+// function mousePressed(){
+//     shapeColor = color(random(255), random(255),random(255))
+//     let x1 = random(width);
+//     let y1 = random(height);
+//     let x2 = random(width);
+//     let y2 = random(height);
+//     let x3 = random(width);
+//     let y3 = random(height);
     
-}
+// }
 
-function mouseReleased(){
-    shapeColor = color(random(255), random(255),random(255))
-}
+// function mouseReleased(){
+//     shapeColor = color(random(255), random(255),random(255))
+// }
