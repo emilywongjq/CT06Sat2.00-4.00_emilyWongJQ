0a473afcef0b93b4983cbcfac0d91334a85c2fdd
Emily Wong <emily.wongjq@gmail.com>
2025-08-16 06:17:03 +0000

    Aug 16, 2025, 6:17 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,11 +1,11 @@
 // write your codes here
-
+let shapecolour = "blue"
 function setup(){
     createCanvas(400, 400);
     background(200);
 }
 
 function draw(){
-    fill(shapecolour)
+    fill(shapecolour);
     circle()
 }
