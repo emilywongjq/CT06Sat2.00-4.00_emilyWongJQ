2659c10d991b4ec79800bbdaec32ecd1480cb57f
Emily Wong <emily.wongjq@gmail.com>
2025-08-23 07:19:35 +0000

    Aug 23, 2025, 7:19 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -33,7 +33,7 @@ function setup(){
 }
 function draw(){
     for(let i = 0 ; i<5 ; i++){
-        circle( 100 + i*100 , 100 + i*100 , 100);
+        circle( 100 + i*100 , 100 + i*100 , 75);
     }
 }
 
