ff7d3ea03e55aebe947dbb7113c099c176a62edc
Emily Wong <emily.wongjq@gmail.com>
2025-08-23 06:25:29 +0000

    Aug 23, 2025, 6:25 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -4,13 +4,12 @@ function setup(){
     createCanvas(400,400)
     background(200);
 }
+function mouseReleased(){
+    shapeColor = color(random(255), random(255),random(255));
+}
 
 function mouseDragged(){
     fill(shapeColour)
     circleSize += 2
     ellipse(mouseX,mouseY,circleSize,circleSize);
-}
-
-function mouseReleased(){
-    shapeColor = color(random(255), random(255),random(255));
 }
