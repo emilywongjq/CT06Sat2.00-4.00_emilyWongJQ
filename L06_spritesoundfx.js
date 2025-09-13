27ff9a0b4e37b277f05f9cd604b8c6bedca23c42
Emily Wong <emily.wongjq@gmail.com>
2025-09-13 07:08:21 +0000

    Sep 13, 2025, 7:08 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -2,6 +2,13 @@ function setup(){
     createCanvas(400,400);
 }
 
+let x = 200;
+let y = 200;
 function draw(){
-    background(220);
+    background(25);
+    square
+    if(keyIsDown(UP_ARROW)){
+        rect()
+    }
+    constrain(x ,100, 300);
 }
