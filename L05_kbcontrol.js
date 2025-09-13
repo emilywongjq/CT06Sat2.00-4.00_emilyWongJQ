3fbf25e80de40afaf383364d5b309e20ac066532
Emily Wong <emily.wongjq@gmail.com>
2025-09-13 06:07:18 +0000

    Sep 13, 2025, 6:07 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -136,7 +136,7 @@ function draw(){
         fill(255,0,0);
     }
     else if (mouseX < 200){
-        fill(0,255,0);
+        fill(0, 255, 89);
     }
     else if (mouseX < 300){
         fill(0,0,255);
