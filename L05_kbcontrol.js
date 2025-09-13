6be14ca7ce6e2f31e63cef4672434302e9f2673d
Emily Wong <emily.wongjq@gmail.com>
2025-09-13 06:03:43 +0000

    Sep 13, 2025, 6:03 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -136,7 +136,7 @@ function draw(){
         fill(250,0,0);
     }
     else if (mouseX < 200){
-        fill();
+        fill(0,250,0);
     }
     else if (mouseX < 300){
         circleColour = 'blue'
